bb44feed7f4056f07b6889f0f98d3d398e974ece
Ali Hakimi <ali.hakimi@colorado.edu>
2014-06-17 21:28:51 -0600

    Fixed Verification Page and added corny buttons

--- src/com/example/gasguzzler/VerificationPage.java
+++ src/com/example/gasguzzler/VerificationPage.java
@@ -8,8 +8,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 public class VerificationPage extends MainActivity {
@@ -17,6 +17,12 @@ public class VerificationPage extends MainActivity {
 	String price;
 	String volume;
 	String odometer;
+	
+	Button back;
+	Button toSummary;
+	EditText etPrice;
+	EditText etVolume;
+	EditText etOdometer;
 	
 	
 	/** Called when the activity is first created. */
@@ -27,10 +33,18 @@ public class VerificationPage extends MainActivity {
        price = getIntent().getStringExtra("STRING_PRICE");
        volume = getIntent().getStringExtra("STRING_VOLUME");
        odometer = getIntent().getStringExtra("STRING_ODOMETER"); 
+       
+       etPrice = (EditText) findViewById(R.id.editText_price);
+       etVolume = (EditText) findViewById(R.id.editText_volume);
+       etOdometer = (EditText) findViewById(R.id.editText_odometer);
+       
+       etPrice.setText(price);
+       etVolume.setText(volume);
+       etOdometer.setText(odometer);
+       
+        Log.i("Before Changing Verifications", " P: " + price + " V: " + volume + " O: " + odometer );
         
-        Log.i("TEST", " P: " + price + " V: " + volume + " O: " + odometer );
-        
-        Button back = (Button) findViewById(R.id.b_backtoOdometer);
+        back = (Button) findViewById(R.id.b_backtoOdometer);
         back.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent();
@@ -40,17 +54,17 @@ public class VerificationPage extends MainActivity {
 
         });
         
-        Button next = (Button) findViewById(R.id.b_toSummary);
-        next.setOnClickListener(new View.OnClickListener() {
+        toSummary = (Button) findViewById(R.id.b_toSummary);
+        toSummary.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
             	
             	Calendar c = Calendar.getInstance();
             	SimpleDateFormat df = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
             	String date = df.format(c.getTime());
             	
-            	double p = Double.parseDouble(price);
-            	double v = Double.parseDouble(volume);
-            	double o = Double.parseDouble(odometer);
+            	double p = Double.parseDouble(etPrice.getText().toString());
+            	double v = Double.parseDouble(etVolume.getText().toString());
+            	double o = Double.parseDouble(etOdometer.getText().toString());
             	
             	
             	database.insertData(p, v, o, date);
