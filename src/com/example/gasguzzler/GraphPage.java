ff62357865972ec8db1cedb7a9c86de991614571
Ali Hakimi <ali.hakimi@colorado.edu>
2014-06-23 22:36:36 -0600

    Fixed Shadow/Text size issue for buttons. Added some better graph functionality.

--- src/com/example/gasguzzler/GraphPage.java
+++ src/com/example/gasguzzler/GraphPage.java
@@ -3,14 +3,14 @@ package com.example.gasguzzler;
 import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
-import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.BarGraphView;
 import com.jjoe64.graphview.GraphView.GraphViewData;
 import com.jjoe64.graphview.GraphViewSeries;
+import com.jjoe64.graphview.GraphViewSeries.GraphViewSeriesStyle;
 import com.jjoe64.graphview.LineGraphView;
 
 public class GraphPage extends Activity {
@@ -30,7 +30,77 @@ public class GraphPage extends Activity {
     	Cursor cur = db.rawQuery(sql, null);
     	//Log.i("CRASH", "Crash 2");
     	cur.moveToFirst();
-    	//Log.i("CRASH", "Crash 3");
+    	
+    	
+    	LineGraphView mpgGraphView = new LineGraphView(this, "Average Miles per Gallon");
+    	mpgGraphView.setVerticalLabels(new String[] { "60", "50", "40", "30",
+                "20", "10", "0"});
+    	mpgGraphView.setHorizontalLabels(new String[] { "January", "February", "March", "April",
+                "May", "June" });
+        
+        GraphViewSeriesStyle seriesStyle = new GraphViewSeriesStyle();
+        GraphViewSeries exampleSeries2 = new GraphViewSeries(
+                "Gas Graph",
+                seriesStyle,
+                new GraphViewData[] 
+                	{ 	new GraphViewData(0, 24d),
+                        new GraphViewData(1, 40d),
+                        new GraphViewData(2, 30d),
+                       // new GraphViewData(.75, 15d)
+                    });
+        mpgGraphView.setManualYAxisBounds(60, 0);
+        mpgGraphView.addSeries(exampleSeries2);
+      //  mpgGraphView.setViewPort(0, 1);
+      // mpgGraphView.setScrollable(true);
+        mpgGraphView.setScalable(true);
+        mpgGraphView.getGraphViewStyle().setGridColor(Color.DKGRAY);
+        mpgGraphView.getGraphViewStyle().setHorizontalLabelsColor(Color.WHITE);
+        mpgGraphView.getGraphViewStyle().setVerticalLabelsColor(Color.WHITE);
+       // graphView.setDrawBackground(true);
+       // graphView.setBackgroundColor(Color.BLUE);
+        
+        LinearLayout layout = (LinearLayout) findViewById(R.id.graph_pageLayout1);  
+    	layout.addView(mpgGraphView); 
+    	
+    	
+    	BarGraphView mpgBarGraph = new BarGraphView(this, "Average Miles per Gallon");
+    	mpgGraphView.setVerticalLabels(new String[] { "60", "50", "40", "30",
+                "20", "10", "0"});
+    	mpgGraphView.setHorizontalLabels(new String[] { "January", "February", "March", "April",
+                "May", "June" });
+    	
+    	
+        
+        GraphViewSeriesStyle barSeriesStyle = new GraphViewSeriesStyle();
+        GraphViewSeries barGraphSeries = new GraphViewSeries(
+                "Gas Graph",
+                barSeriesStyle,
+                new GraphViewData[] 
+                	{ 	new GraphViewData(0, 25),
+                        new GraphViewData(5, 55d),
+                       // new GraphViewData(2, 30d),
+                       // new GraphViewData(.75, 15d)
+                    });
+       // mpgGraphView.setManualYAxisBounds(60, 0);
+        mpgBarGraph.addSeries(barGraphSeries);
+        mpgBarGraph.setViewPort(0, 40);
+       // mpgBarGraph.setScrollable(true);
+       // mpgBarGraph.setScalable(true);
+    
+        mpgBarGraph.getGraphViewStyle().setGridColor(Color.DKGRAY);
+        mpgBarGraph.getGraphViewStyle().setNumHorizontalLabels(6);
+        mpgBarGraph.getGraphViewStyle().setNumVerticalLabels(6);
+        mpgBarGraph.getGraphViewStyle().setHorizontalLabelsColor(Color.WHITE);
+        mpgBarGraph.getGraphViewStyle().setVerticalLabelsColor(Color.WHITE);
+       // graphView.setDrawBackground(true);
+       // graphView.setBackgroundColor(Color.BLUE);
+        
+        LinearLayout barGraphLayout = (LinearLayout) findViewById(R.id.graph_pageLayout2);  
+    	barGraphLayout.addView(mpgBarGraph); 
+    	
+        
+        
+    	/*
     	
         GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {  
         	      
@@ -60,8 +130,13 @@ public class GraphPage extends Activity {
         	GraphView graphView = new LineGraphView(this, "Gas Data");  
         	graphView.addSeries(exampleSeries); // data  
         	  
-        	LinearLayout layout = (LinearLayout) findViewById(R.id.graph_pageLayout);  
-        	layout.addView(graphView); 
-      
+        	
+      */
+        	
     }
+    
+    @Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
 }
